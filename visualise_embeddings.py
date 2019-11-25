f37e6651d26a101e86fbaa9898e0b4999ee969c6
Bartek P <bar.paszko@gmail.com>
2019-11-25 01:13:52 +0100

    neural net + loading data from split

--- visualise_embeddings.py
+++ visualise_embeddings.py
@@ -6,7 +6,7 @@ import os
 
 if __name__ == '__main__':
     # Load corpus
-    corpus_dir = './categorization/learningData/'
+    corpus_dir = './data/GAZETA_zawody/test'
     x = Embeder(corpus_dir)
     
     # Load model
@@ -16,10 +16,11 @@ if __name__ == '__main__':
     # Compute embeddings
     contexts = ['one-mention', 'document', 'corpus']
     neighborhoods = [2, 3, 4, 5, 'sentence']
-    for context, neighborhood in list(itertools.product(contexts, neighborhoods))[10:]:  # modify range
-        save_dir = f'../projections/elmo_{context}_{str(neighborhood)}/'
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-            
-        embeds = x.get_embeddings(context, neighborhood, model)
-        x.save_to_tensorboard(embeds, save_dir)
+    # for k in list(itertools.product(contexts, neighborhoods)):  # modify range
+    context, neighborhood = 'corpus', 'sentence'
+    save_dir = f'./projections/gazeta/elmo_{context}_{str(neighborhood)}/validation'
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+        
+    embeds = x.get_embeddings(context, neighborhood, model)
+    x.save_to_tensorboard(embeds, save_dir)
